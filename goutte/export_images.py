e0b601bda5f85c773b4c0edde71258e029858f52
Malo Kerebel <malo.kerebel@laposte.net>
2021-04-07 21:35:53 +0200

    added speed computation

--- goutte/export_images.py
+++ goutte/export_images.py
@@ -11,6 +11,52 @@ import matplotlib.animation as animation
 
 import os
 
+def get_center(phi_at_t, treshold):
+    """ gets the barycenter of phi, over a chosen threshold """
+    sum_phix = 0
+    sum_phiy = 0
+    sum_phi = 0
+    for i in range(len(phi_at_t[:,0])):
+        for j in range(len(phi_at_t[0,:])):
+            if phi_at_t[i,j] > treshold:
+                # i and j are in the wrong order because of how we obtain phi
+                sum_phiy += max_x * j / len(x)  * phi_at_t[i,j]
+                # we normalize j or i an integer, to the real distance between two points
+                sum_phix += max_y * i / len(y)  * phi_at_t[i,j]
+                sum_phi += phi_at_t[i,j]
+
+    return sum_phix/ sum_phi, sum_phiy/ sum_phi
+
+def get_min_max_phi(phi_at_t, threshold):
+    """ outputs the y minimum and maximum of phi if its above a given treshold """
+    y_min = 100
+    y_max = 0
+
+    for i in range(len(phi_at_t[0,:])):
+        # We iterate over all possible x to find the max of y and its min
+        max_done = False
+        iterate = True
+        j = len(phi_at_t[:,0]) - 1
+        while iterate and j > 0:
+            # We iterate over j, until we have found the minimum and the maximum (or if we found nothing above the threshold
+            if max_done == False and phi_at_t[j, i] > threshold:
+
+                if y_max < max_y * j / len(y):
+                    y_max = max_y * j / len(y)
+                max_done = True
+
+            #if max_done:
+                #print(phi_at_t[j, i])
+
+            if max_done and phi_at_t[j, i] < threshold:
+
+                if y_min > max_y * (j+1) / len(y):
+                    y_min = max_y * (j+1) / len(y)
+                iterate = False
+            j -= 1
+
+    return y_min, y_max
+
 enregistrer = False
 
 home = os.environ['HOME']
@@ -37,20 +83,26 @@ phi = f.variables['tracer']
 # Indice de l'image
 i = 200
 
-x = np.linspace(0,1,np.shape(phi[1, :, :].T)[1])
-y = np.linspace(0,2,np.shape(phi[1, :, :].T)[0])
+max_x = 1
+max_y = 2
+
+x = np.linspace(0,max_x,np.shape(phi[1, :, :].T)[1])
+y = np.linspace(0,max_y,np.shape(phi[1, :, :].T)[0])
 X, Y = np.meshgrid(x,y)
 t = np.ravel(f.variables['t'])
 
+x_center, y_center = get_center(phi[i, :, :].T, 0.8)
 
 plt.figure('goutte', figsize=(5, 10))
 plt.clf()
 plt.pcolormesh(X, Y, np.flipud(phi[i, :, :].T), cmap='inferno', shading='gouraud')
+plt.colorbar()
+#plt.plot(y_center, 2 - x_center, "ro")
 titre = "t="+str(round(t[i],3))+"s"
 plt.title(titre)
 plt.tight_layout()
 
-plt.show()
+#plt.show()
 
 if enregistrer:
     cmap = 'inferno'
@@ -74,3 +126,44 @@ if enregistrer:
     
     file_name = 'gtte.mp4'
     anim.save(file_name, writer= writer )
+
+v = [0]
+v_x = [0]
+v_y = [0]
+
+prev_x, prev_y = get_center(phi[0, :, :], 0.8)
+
+#Computation of different speed
+for i in range(1,len(t)):
+    print(i, "/", len(t)) # Can be used to show progress
+    x_i, y_i = get_center(phi[i, :, :], 0.8)
+    v_x.append((x_i - prev_x) / (t[i] - t[i-1]))
+    v_y.append((y_i - prev_y) / (t[i] - t[i-1]))
+    v.append((np.sqrt((x_i - prev_x)**2 + (y_i - prev_y)**2)) / (t[i] - t[i-1]))
+    prev_x = x_i
+    prev_y = y_i
+
+plt.figure("Speed")
+plt.plot(t, v, label = "Total speed")
+plt.plot(t, v_x, label = "Vx")
+plt.plot(t, v_y, label = "Vy")
+plt.title("Speed of the bubble")
+plt.xlabel("Time")
+plt.ylabel("Speed")
+plt.legend()
+plt.show()
+
+oscilation = []
+
+for i in range(len(t)):
+    print(i, "/", len(t)) # Can be used to show progress
+    y_min_i, y_max_i = get_min_max_phi(phi[i, :, :].T, 0.97)
+    print(y_max_i, y_min_i, y_max_i - y_min_i)
+    oscilation.append(y_max_i - y_min_i)
+
+plt.figure("oscillation")
+plt.plot(t, oscilation)
+plt.ylabel("Height")
+plt.xlabel("time")
+plt.title("Height of the buble in time")
+plt.show()
