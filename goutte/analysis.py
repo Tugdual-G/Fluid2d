90af3e44df34077c92a27e7d49548034adb2cfe4
Malo Kerebel <malo.kerebel@laposte.net>
2021-04-14 22:54:11 +0200

    PEP8 change

--- goutte/analysis.py
+++ goutte/analysis.py
@@ -11,38 +11,42 @@ import matplotlib.pyplot as plt
 
 import os
 
+
 def get_center(phi_at_t, treshold):
     """ gets the barycenter of phi, over a chosen threshold """
     sum_phix = 0
     sum_phiy = 0
     sum_phi = 0
-    for i in range(len(phi_at_t[:,0])):
-        for j in range(len(phi_at_t[0,:])):
-            if phi_at_t[i,j] > treshold:
+    for i in range(len(phi_at_t[:, 0])):
+        for j in range(len(phi_at_t[0, :])):
+            if phi_at_t[i, j] > treshold:
                 # i and j are in the wrong order because of how we obtain phi
-                sum_phiy += y[i]  * phi_at_t[i,j]
-                # we normalize j or i an integer, to the real distance between two points
-                sum_phix += x[j]  * phi_at_t[i,j]
-                sum_phi += phi_at_t[i,j]
+                sum_phiy += y[i] * phi_at_t[i, j]
+                sum_phix += x[j] * phi_at_t[i, j]
+                sum_phi += phi_at_t[i, j]
 
-    return sum_phix/ sum_phi, sum_phiy/ sum_phi
+    return sum_phix / sum_phi, sum_phiy / sum_phi
+
 
 def get_min_max_phi(phi_at_t, threshold, index_x):
-    """ outputs the y minimum and maximum of phi if its above a given treshold """
+    """ outputs the y minimum and maximum of phi if its above a given treshold
+    """
     y_min = 100
     y_max = 0
     max_done = False
     iterate = True
-    j = len(phi_at_t[:,0])-1
+    j = len(phi_at_t[:, 0])-1
 
     while iterate and j > 0:
-        # We iterate over j, until we have found the minimum and the maximum (or if we found nothing above the threshold
-        if max_done == False and phi_at_t[j, index_x] > threshold:
-            y_max = y[j] 
+        # We iterate over j, until we have found the minimum and the maximum
+        # We should always find a value because we take the index of the center
+        # but the j > 0 avoids any crash
+        if max_done is False and phi_at_t[j, index_x] > threshold:
+            y_max = y[j]
             max_done = True
 
-        #if max_done:
-            #print(phi_at_t[j, i])
+        # f max_done:
+            # rint(phi_at_t[j, i])
 
         if max_done and phi_at_t[j, index_x] < threshold:
 
@@ -52,10 +56,12 @@ def get_min_max_phi(phi_at_t, threshold, index_x):
 
     return y_min, y_max
 
+
 home = os.environ['HOME']
-#path = "/data/fluid2d/bien"
+# path = "/data/fluid2d/bien"
 path = "/data/fluid2d"
-print(os.listdir(home + path)) # The name of the dirs are the name of the experiments
+print(os.listdir(home + path))
+# The name of the dirs are the name of the experiments
 
 tries = 0
 
@@ -68,13 +74,13 @@ while tries < 3:
         print("Incorrect input")
         tries += 1
         if tries > 2:
-            exit(0) # If we got too many errors we exit
+            exit(0)  # If we got too many errors we exit
 
-#print(f)
+# print(f)
 print(f.variables.keys())
 
 phi = f.variables['phi']
-#print(phi)
+# print(phi)
 # Indice de l'image
 
 #%%
@@ -86,19 +92,19 @@ max_y = np.amax(f.variables['x'])
 
 # print(max_x, max_y)
 
-x = np.linspace(0,max_x,np.shape(phi[1, :, :].T)[1])
-y = np.linspace(0,max_y,np.shape(phi[1, :, :].T)[0])
+x = np.linspace(0, max_x, np.shape(phi[1, :, :].T)[1])
+y = np.linspace(0, max_y, np.shape(phi[1, :, :].T)[0])
 
 dx = max_x / len(x)
 dy = max_y / len(y)
 
-X, Y = np.meshgrid(x,y)
+X, Y = np.meshgrid(x, y)
 t = np.ravel(f.variables['t'])
 
 x_center, y_center = get_center(phi[i, :, :].T, 0.8)
 
-indices_x = np.argwhere((x > x_center - dx) * ( x < x_center + dx))
-indices_y = np.argwhere((y > y_center - dy) * ( y < y_center + dy))
+indices_x = np.argwhere((x > x_center - dx) * (x < x_center + dx))
+indices_y = np.argwhere((y > y_center - dy) * (y < y_center + dy))
 
 y_min, y_max = get_min_max_phi(phi[i, :, :].T, 0.8, indices_x[0][0])
 hauteur = y_max - y_min
@@ -113,7 +119,7 @@ plt.plot(x_center, y[-1]-y_center, "ro")
 plt.axhline(max_y-y_max)
 plt.axhline(max_y-y_min)
 print(x_center, y_center)
-titre = "t="+str(round(t[i],3))+"s"
+titre = "t="+str(round(t[i], 3))+"s"
 plt.title(titre)
 plt.tight_layout()
 
@@ -131,18 +137,20 @@ oscillation = [hauteur/largeur]
 list_y_max = []
 list_y_min = []
 
-#Computation of different speed
-for i in range(1,len(t)):
-    print(i, "/", len(t)) # Can be used to show progress
-    x_i, y_i = get_center(phi[i, :, :].T, 0.8) #.T should be removed in case of a change in the direction of g in the experiment
-    v_x.append((x_i - prev_x) / (t[i] - t[i-1])) # get speed by the difference of position / difference in time
+# Computation of different speed
+for i in range(1, len(t)):
+    print(i, "/", len(t))  # Can be used to show progress
+    x_i, y_i = get_center(phi[i, :, :].T, 0.8)
+    # .T should be removed in case of a change in the direction of g in the experiment
+    v_x.append((x_i - prev_x) / (t[i] - t[i-1]))
+    # get speed by the difference of position / difference in time
     v_y.append((y_i - prev_y) / (t[i] - t[i-1]))
     v.append((np.sqrt((x_i - prev_x)**2 + (y_i - prev_y)**2)) / (t[i] - t[i-1]))
     prev_x = x_i
     prev_y = y_i
-    
-    indices_x = np.argwhere((x > x_i - dx) * ( x < x_i + dx))
-    indices_y = np.argwhere((y > y_i - dy) * ( y < y_i + dy))
+   
+    indices_x = np.argwhere((x > x_i - dx) * (x < x_i + dx))
+    indices_y = np.argwhere((y > y_i - dy) * (y < y_i + dy))
 
     y_min, y_max = get_min_max_phi(phi[i, :, :].T, 0.8, indices_x[0][0])
     hauteur = y_max - y_min
@@ -163,18 +171,18 @@ np.save(home + path + '/' + fold + '/' + fold + 'y_min.npy', list_y_min)
 
 
 #%%
-#oscilation = []
-#list_y_max = []
-#list_y_min = []
+# oscilation = []
+# list_y_max = []
+# list_y_min = []
 
-#for i in range(len(t)):
-    #print(i, "/", len(t)) # Can be used to show progress
-    #y_min_i, y_max_i = get_min_max_phi(phi[i, :, :].T, 0.95) #.T should be removed in case of a change in the direction of g in the experiment
-    ## print(y_max_i, y_min_i, y_max_i - y_min_i)
-    #oscilation.append(y_max_i - y_min_i)
-    #list_y_max += [y_max]
-    #list_y_min += [y_min]
+# for i in range(len(t)):
+    # print(i, "/", len(t)) # Can be used to show progress
+    # y_min_i, y_max_i = get_min_max_phi(phi[i, :, :].T, 0.95) #.T should be removed in case of a change in the direction of g in the experiment
+    # # print(y_max_i, y_min_i, y_max_i - y_min_i)
+    # oscilation.append(y_max_i - y_min_i)
+    # list_y_max += [y_max]
+    # list_y_min += [y_min]
 
-#np.save(home + path + '/' + fold + '/' + fold + 'oscilations.npy', oscilation)
-#np.save(home + path + '/' + fold + '/' + fold + 'y_max.npy', list_y_max)
-#np.save(home + path + '/' + fold + '/' + fold + 'y_min.npy', list_y_min)
+# np.save(home + path + '/' + fold + '/' + fold + 'oscilations.npy', oscilation)
+# np.save(home + path + '/' + fold + '/' + fold + 'y_max.npy', list_y_max)
+# np.save(home + path + '/' + fold + '/' + fold + 'y_min.npy', list_y_min)
--- goutte/export_images.py
+++ goutte/export_images.py
@@ -9,93 +9,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import os
-plt.rc('text', usetex = True)
-plt.rc('font', family = 'serif')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 enregistrer = False
 
 home = os.environ['HOME']
 path = "/data/fluid2d"
-print(os.listdir(home + path)) # The name of the dirs are the name of the experiments
+print(os.listdir(home + path))
+# The name of the dirs are the name of the experiments
 
-#fold = "test_analysis"
-#f = Dataset(home + path +'/' + fold + '/' + fold + '_his.nc')
+# fold = ""
+# f = Dataset(home + path +'/' + fold + '/' + fold + '_his.nc')
 
 tries = 0
 
 while tries < 3:
     try:
         fold = input("Enter the experiment you want to see\n")
-        f = Dataset(home + path +'/' + fold + '/' + fold + '_his.nc')
+        f = Dataset(home + path + '/' + fold + '/' + fold + '_his.nc')
         break
     except:
         print("Incorrect input")
         tries += 1
         if tries > 2:
-            exit(0) # If we got too many errors we exit
+            exit(0)  # If we got too many errors we exit
 
 #%%
-#print(f)
-#print(f.variables.keys())
+# print(f)
+# print(f.variables.keys())
 
 phi = f.variables['tracer']
-#print(phi)
-# Indice de l'image
-i = 0
+# print(phi)
+
 
 max_x = np.amax(f.variables['y'])
 max_y = np.amax(f.variables['x'])
-x = np.linspace(0,max_x,np.shape(phi[1, :, :].T)[1])
-y = np.linspace(0,max_y,np.shape(phi[1, :, :].T)[0])
-X, Y = np.meshgrid(x,y)
+x = np.linspace(0, max_x, np.shape(phi[1, :, :].T)[1])
+y = np.linspace(0, max_y, np.shape(phi[1, :, :].T)[0])
+X, Y = np.meshgrid(x, y)
 t = np.ravel(f.variables['t'])
 
-y_max = np.load(home + path +'/' + fold + '/' + fold + 'y_max.npy')
-y_min = np.load(home + path +'/' + fold + '/' + fold + 'y_min.npy')
+# Indice de l'image
+i = 2 * len(t) // 3
+
+y_max = np.load(home + path + '/' + fold + '/' + fold + 'y_max.npy')
+y_min = np.load(home + path + '/' + fold + '/' + fold + 'y_min.npy')
 
 plt.figure('goutte', figsize=(5, 10))
 plt.clf()
 plt.pcolormesh(X, Y, np.flipud(phi[i, :, :].T), cmap='inferno', shading='gouraud')
 plt.colorbar()
-#plt.axhline(max_y-y_max[i])
-#plt.axhline(max_y-y_min[i])
-titre = "t="+str(round(t[i],3))+"s"
+# plt.axhline(max_y-y_max[i])
+# plt.axhline(max_y-y_min[i])
+titre = "t="+str(round(t[i], 3))+"s"
 plt.title(titre)
 plt.tight_layout()
-#plt.show()
+# plt.show()
 
-#plt.savefig('goutte_non-miscible.pdf', dpi=300)
+# plt.savefig('goutte_non-miscible.pdf', dpi=300)
 #%%
 y_max = np.load(home + path + '/' + fold + '/' + fold + 'y_max.npy')
 y_min = np.load(home + path + '/' + fold + '/' + fold + 'y_min.npy')
 if enregistrer:
     cmap = 'inferno'
     shade = 'gouraud'
-    #Préparation de l'affichage pour enregistrement
-    fig1, ax1 = plt.subplots(num = 'animation', dpi = 200, figsize = (4,12))     
+    # Préparation de l'affichage pour enregistrement
+    fig1, ax1 = plt.subplots(num='animation', dpi=200, figsize=(4, 12))
     plt.axis('off')
-    plt.tight_layout(pad = 0)
-    img = ax1.pcolormesh(X,Y,np.flipud(phi[0, :, :].T), cmap = cmap, shading= shade) 
+    plt.tight_layout(pad=0)
+    img = ax1.pcolormesh(X, Y, np.flipud(phi[0, :, :].T), cmap=cmap, shading=shade)
     img1 = ax1.axhline(max_y-y_max[0])
     img2 = ax1.axhline(max_y-y_min[0])
-    
+
     print(int(len(t)/t[-1]))
-    
-    writer = animation.FFMpegWriter(fps = 5, bitrate = 5000)
-    def diapo(n):		
+
+    writer = animation.FFMpegWriter(fps=5, bitrate=5000)
+
+    def diapo(n):
         global img, img1, img2
         img.remove()
         img1.remove()
-        img2.remove()             
-        img = ax1.pcolormesh(X,Y,np.flipud(phi[n, :, :].T),cmap = cmap, shading = shade) 
+        img2.remove()            
+        img = ax1.pcolormesh(X, Y, np.flipud(phi[n, :, :].T), cmap=cmap, shading=shade)
         img1 = ax1.axhline(max_y-y_max[n])
-        img2 = ax1.axhline(max_y-y_min[n])	
-    
-    #Création de l'animation
+        img2 = ax1.axhline(max_y-y_min[n])
+
+    # Création de l'animation
     anim = animation.FuncAnimation(fig1, diapo, frames=50)
-    
-    file_name = 'longcouloir.mp4'
-    anim.save(file_name, writer= writer )
+
+    file_name = fold + '.mp4'
+    anim.save(file_name, writer=writer)
 
 v = [0]
 v_x = [0]
@@ -123,41 +127,38 @@ ax4 = plt.subplot2grid((4,4), (0,3), colspan=1, rowspan=3, sharey=ax3, frameon=F
 plt.pcolormesh(X, Y, np.flipud(phi[-1, :, :].T), cmap='inferno', shading='gouraud')
 plt.subplot2grid((4,4), (3,0), colspan=4, rowspan=1)"""
 
-fig, ax = plt.subplots(2,1, sharex = True)
+fig, ax = plt.subplots(2, 1, sharex=True)
 
-#plt.plot(t, v, 'k', linewidth=0.5, label = "Total speed")
-ax[0].plot(t, v_x, 'b', linewidth=0.5, label = "$V_x$")
-ax[0].plot(t, v_y, 'k', linewidth=0.5, label = "$V_y$")
+# plt.plot(t, v, 'k', linewidth=0.5, label = "Total speed")
+ax[0].plot(t, v_x, 'b', linewidth=0.5, label="$V_x$")
+ax[0].plot(t, v_y, 'k', linewidth=0.5, label="$V_y$")
 ax[0].set_title("Speed of the bubble")
-ax[0].set_xlabel("t")
 ax[0].set_ylabel("V")
 ax[0].grid()
-#ax[0].tight_layout()
+# ax[0].tight_layout()
 ax[0].legend()
-#plt.show()
+# plt.show()
 
 # plt.figure("oscillation")
-#plt.clf()
+# plt.clf()
 ax[1].plot(t, oscillation, 'k', linewidth=0.5)
 ax[1].set_ylabel("Height")
 ax[1].set_xlabel("time")
-ax[1].set_title("Height of the buble in time")
-
-print(oscillation)
+ax[1].set_title("Ratio height/width of the bubble")
 
 plt.show()
 
 #%%
 phi = f.variables['phi']
 nmbr = 5
-fig1, ax = plt.subplots(1,nmbr, num = 'snap', clear = True, sharey=True,
-                        gridspec_kw = {'hspace': 0.05,'wspace': 0.2 })
+fig1, ax = plt.subplots(1, nmbr, num='snap', clear=True, sharey=True,
+                        gridspec_kw={'hspace': 0.05, 'wspace': 0.2})
 i = 0
 t_step = len(t)//nmbr
 for i in range(nmbr):
     ax[i].pcolormesh(X, Y, np.flipud(phi[t_step*i, :, :].T), cmap='inferno', shading='gouraud')
-    ax[i].set_ylim(0.5,3)
-    titre = "t="+str(round(t[t_step*i],3))+" s"
+    ax[i].set_ylim(0.5, 3)
+    titre = "t="+str(round(t[t_step*i], 3))+" s"
     ax[i].set_title(titre)
 ax[2].set_xlabel('x')
 ax[0].set_ylabel('y')
