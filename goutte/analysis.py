fixed ellipticity computation

goutte/analysis.py
@@ -35,20 +35,20 @@ def get_min_max_phi(phi_at_t, threshold, index_x):
     y_max = 0
     max_done = False
     iterate = True
-    j = len(phi_at_t[:, 0])-1
+    j = len(phi_at_t[:,  index_x])-1
 
     while iterate and j > 0:
         # We iterate over j, until we have found the minimum and the maximum
         # We should always find a value because we take the index of the center
         # but the j > 0 avoids any crash
-        if max_done is False and phi_at_t[j, index_x] < threshold:
+        if max_done is False and phi_at_t[j, index_x] > threshold:
             y_max = y[j]
             max_done = True
 
         # f max_done:
             # rint(phi_at_t[j, i])
 
-        if max_done and phi_at_t[j, index_x] > threshold:
+        if max_done and phi_at_t[j, index_x] < threshold:
 
             y_min = y[j+1]
             iterate = False
@@ -59,9 +59,9 @@ def get_min_max_phi(phi_at_t, threshold, index_x):
 
 home = os.environ['HOME']
 
-# path = "/data/fluid2d/bien"
-path = "/data/fluid2d"
-print(os.listdir(home + path))# The name of the dirs are the name of the experiments
+path = "/data/fluid2d/bien"
+# path = "/data/fluid2d"
+print(os.listdir(home + path))  # The name of the dirs are the name of the experiments
 
 tries = 0
 
@@ -77,7 +77,7 @@ while tries < 3:
             exit(0)  # If we got too many errors we exit
 
 # print(f)
-print(f.variables.keys())
+# print(f.variables.keys())
 
 phi = f.variables['phi']
 # print(phi)
@@ -116,9 +116,10 @@ plt.clf()
 plt.pcolormesh(X, Y, np.flipud(phi[i, :, :].T), cmap='inferno', shading='gouraud')
 plt.colorbar()
 plt.plot(x_center, y[-1]-y_center, "ro")
+# print(max_y, y_max, y_min)
 plt.axhline(max_y-y_max)
 plt.axhline(max_y-y_min)
-print(x_center, y_center)
+# print(x_center, y_center)
 titre = "t="+str(round(t[i], 3))+"s"
 plt.title(titre)
 plt.tight_layout()
@@ -133,7 +134,14 @@ v_y = [0]
 
 prev_x, prev_y = get_center(phi[0, :, :].T, 0.8)
 
-oscillation = [hauteur/largeur]
+if hauteur > largeur:
+    a = hauteur
+    b = largeur
+else:
+    a = largeur
+    b = hauteur
+
+ellipticity = [1 - b/a]
 list_y_max = []
 list_y_min = []
 
@@ -148,7 +156,7 @@ for i in range(1, len(t)):
     v.append((np.sqrt((-x_i + prev_x)**2 + (-y_i + prev_y)**2)) / (t[i] - t[i-1]))
     prev_x = x_i
     prev_y = y_i
-   
+
     indices_x = np.argwhere((x > x_i - dx) * (x < x_i + dx))
     indices_y = np.argwhere((y > y_i - dy) * (y < y_i + dy))
 
@@ -157,7 +165,14 @@ for i in range(1, len(t)):
     x_min, x_max = get_min_max_phi(phi[i, :, :], 0.8, indices_y[0][0])
     largeur = x_max - x_min
 
-    oscillation.append(hauteur/largeur)
+    if hauteur > largeur:
+        a = hauteur
+        b = largeur
+    else:
+        a = largeur
+        b = hauteur
+
+    ellipticity.append(1 - b/a)
     list_y_max += [y_max]
     list_y_min += [y_min]
 
@@ -165,7 +180,7 @@ np.save(home + path + '/' + fold + '/' + fold + 'velocity.npy', v)
 np.save(home + path + '/' + fold + '/' + fold + 'velocity_x.npy', v_x)
 np.save(home + path + '/' + fold + '/' + fold + 'velocity_y.npy', v_y)
 
-np.save(home + path + '/' + fold + '/' + fold + 'oscillations.npy', oscillation)
+np.save(home + path + '/' + fold + '/' + fold + 'ellipticity.npy', ellipticity)
 np.save(home + path + '/' + fold + '/' + fold + 'y_max.npy', list_y_max)
 np.save(home + path + '/' + fold + '/' + fold + 'y_min.npy', list_y_min)
 

goutte/export_images.py
@@ -13,27 +13,26 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 
-
-def average(x,n):
+def average(x, n):
     X = np.zeros((2*n+1, np.shape(x)[0]+2*n))
     for i in range(n*2):
-        X[i+1,0:i+1] = np.nan
+        X[i+1, 0:i+1] = np.nan
         X[0:-i-1, (-i-1)] = np.nan
     for i in range(2*n+1):
         X[i, i:i+len(x)] = x
-    print(X)
-    return np.nanmean(X,axis=0,keepdims=False)[n:-n]
-    
-    
+    # print(X)
+    return np.nanmean(X, axis=0, keepdims=False)[n:-n]
+
+
 #%%
-        
+
 
 enregistrer = False
 
 home = os.environ['HOME']
 
 path = "/data/fluid2d/bien"
-print(os.listdir(home + path))# The name of the dirs are the name of the experiments
+print(os.listdir(home + path))  # The name of the dirs are the name of the experiments
 
 # fold = ""
 # f = Dataset(home + path +'/' + fold + '/' + fold + '_his.nc')
@@ -65,9 +64,9 @@ x = np.linspace(0, max_x, np.shape(phi[1, :, :].T)[1])
 y = np.linspace(0, max_y, np.shape(phi[1, :, :].T)[0])
 X, Y = np.meshgrid(x, y)
 t = np.ravel(f.variables['t'])
-i_t = np.nonzero(np.abs(t-0.6)<0.01)[0]
+i_t = np.nonzero(np.abs(t-0.6) < 0.01)[0]
 i = i_t[0]
-print(i)
+# print(i)
 
 
 plt.figure('goutte')
@@ -76,12 +75,12 @@ plt.clf()
 plt.pcolormesh(X, Y, np.flipud(phi[i, :, :].T), cmap='inferno', shading='gouraud')
 plt.colorbar(label='Densité de tracer')
 plt.xlabel('x')
-#plt.ylabel('y')
+# plt.ylabel('y')
 # plt.axhline(max_y-y_max[i])
 # plt.axhline(max_y-y_min[i])
 axes = plt.gca()
 axes.set_aspect('equal', 'box')
-plt.yticks([0,0.25,0.5,0.75,1,1.25,1.5,1.75],['','','','','','','',''])
+plt.yticks([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75],['', '', '', '', '', '', '', ''])
 titre = "t="+str(round(t[i], 3))+"s"
 plt.title(titre)
 plt.tight_layout(pad=1)
@@ -111,7 +110,7 @@ if enregistrer:
         global img, img1, img2
         img.remove()
         img1.remove()
-        img2.remove()            
+        img2.remove()
         img = ax1.pcolormesh(X, Y, np.flipud(phi[n, :, :].T), cmap=cmap, shading=shade)
         img1 = ax1.axhline(max_y-y_max[n])
         img2 = ax1.axhline(max_y-y_min[n])
@@ -133,34 +132,36 @@ v_y = [0]
 
 v_x = np.load(home + path + '/' + fold + '/' + fold + 'velocity_x.npy')
 v_y = np.load(home + path + '/' + fold + '/' + fold + 'velocity_y.npy')
-v_x = average(v_x,40)
-v_y = average(v_y,40)
+v_x = average(v_x, 40)
+v_y = average(v_y, 40)
 
 
-oscillation = np.load(home + path + '/' + fold + '/' + fold + 'oscillations.npy')
-oscillation = average(oscillation,10)
-fig, ax = plt.subplots(2,1, num='speed', sharex = True, clear=True)
+ellipticity = np.load(home + path + '/' + fold + '/' + fold + 'ellipticity.npy')
+ellipticity = average(ellipticity, 10)
+fig, ax = plt.subplots(2, 1, num='speed', sharex=True, clear=True)
 
-ax[0].plot(t, v_x, 'b', linewidth=0.5, label = "$V_x$")
-ax[0].plot(t, v_y, 'k', linewidth=0.5, label = "$V_y$")
+ax[0].plot(t, v_x, 'b', linewidth=0.5, label="$V_x$")
+ax[0].plot(t, v_y, 'k', linewidth=0.5, label="$V_y$")
 ax[0].set_title("Vitesse de la bulle", loc='left')
 ax[0].set_ylabel("V")
-#ax[0].set_xlabel("t")
+# ax[0].set_xlabel("t")
 ax[0].grid()
 ax[0].legend()
 # plt.show()
 
 # plt.figure("oscillation")
 # plt.clf()
-ax[1].plot(t, 1-oscillation, 'k', linewidth=0.5)
+ax[1].plot(t, ellipticity, 'k', linewidth=0.5)
 ax[1].grid()
-ax[1].set_ylabel("1-H/L")
+ax[1].set_ylabel(r"\(\varepsilon\)")
 ax[1].set_xlabel("t")
 ax[1].set_title("Ellipticité", loc='left', pad=5)
 
 plt.show()
-fig.savefig(home + path + '/' + fold + '/' + 'oscill.pdf', dpi = 300)
+fig.savefig(home + path + '/' + fold + '/' + 'oscill.pdf', dpi=300)
+
 #%%
+
 phi = f.variables['phi']
 nmbr = 5
 fig1, ax = plt.subplots(1, nmbr, num='snap', clear=True, sharey=True,
@@ -174,5 +175,5 @@ for i in range(nmbr):
     ax[i].axis('equal')
 ax[2].set_xlabel('x')
 ax[0].set_ylabel('y')
-#fig.colorbar(pcm, ax=ax[-1], label='Densité de tracer', )
-fig1.savefig(home + path + '/' + fold + '/' + 'snapshot.pdf', dpi = 300)
+# fig.colorbar(pcm, ax=ax[-1], label='Densité de tracer', )
+fig1.savefig(home + path + '/' + fold + '/' + 'snapshot.pdf', dpi=300)